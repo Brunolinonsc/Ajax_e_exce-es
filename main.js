9356585ab095b810f13d94fefa762871354527d3
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-02-12 21:20:39 -0300

    ok
    12/02/2025

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ document.addEventListener('DOMContentLoaded', function(){
     })
 })
     */
+   //     $.ajax(endpoint).done(function(resposta){
+    //         const logradouro = resposta.logradouro;
+    //         const bairro = resposta.bairro;
+    //         const cidade = resposta.cidade;
+    //         const estado = resposta.uf;
+    //         const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+    //         $('#endereco').val(endereco)
+
+
+    //         setTimeout(function(){
+    //             $(botao).find('i').removeClass('d-none');
+    //             $(botao).find('span').addClass('d-none');
+    //         }, 5000)
+
+
+            
+
+    //     })
+
 
 
 $(document).ready(function(){
@@ -23,26 +42,36 @@ $(document).ready(function(){
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
         const botao = $(this);
-        $(this).find('i').addClass('d-none');
-        $(this).find('span').removeClass('d-none');
-
-        $.ajax(endpoint).done(function(resposta){
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.cidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
-            $('#endereco').val(endereco)
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
 
 
-            setTimeout(function(){
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 5000)
-
-
-            
-
+    fetch(endpoint)
+    .then(function(resposta){
+        return resposta.json();
+    })
+    .then(function(json){
+        const logradouro = json.logradouro;
+        const bairro = json.bairro;
+        const cidade = json.cidade;
+        const estado = json.uf;
+        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+        $('#endereco').val(endereco);
         })
+        
+    })
+    .catch(function(erro){
+        alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+    })
+    .finally(function(){
+        setTimeout(function(){
+            $(botao).find('i').removeClass('d-none');
+            $(botao).find('span').addClass('d-none');
+        }, 1000);
+    })
+    $('#formulario-pedido').submit(function(){
+        if ($('#nome').val().length == 0 ){
+            throw new Error('Digite o nome');
+        }
     })
 })
